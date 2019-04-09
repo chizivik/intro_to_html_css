9bb878a64a1085ec4a99522956aee39bba1ca6be
victor <chukwuziterevictor@gmail.com>
2019-04-09 15:17:37 -0700

    initial commit

---
+++ script.js
@@ -0,0 +1,169 @@
+// Ex on variable declaration
+// let userName = "I am Js"; 
+// alert(userName)
+
+//Ex on incrementation using unary operator
+// let a = 1, b = 1; 
+// alert(++a)
+// alert(b++)
+// alert(b)
+
+//Ex on binary operator
+// let a = 2; 
+// let x = 1 + (a *= 2)
+// alert(x)
+
+// //Ex on interaction using prompt  
+// let age = prompt("please input your age");
+// alert(`you are ${age} years old` );
+
+//Ex on interaction using confirm
+// let isBoss = confirm("Are you the Boss?");
+// alert(isBoss);
+
+//Ex on conditional operator 
+// let year = prompt('what year was javascript created?');
+// if  (year == 2015) {
+//      alert("you are smart");
+//  }
+// else{
+//     alert('you so wrong');
+//  }
+
+//Ex on else if condition
+// let year = prompt('what year was javascript created?');
+// if  (year < 2015){
+//      alert("too early");
+// }else if (year > 2015){
+//     alert('too late');
+// }else {  
+//     alert("exactly");
+// }  
+
+// if (age < 3){
+// let message = 'hi baby!';
+// }else if (age < 18){
+// message = 'hello!';
+// }else if (age < 100){
+// message = 'greetings';
+// }else {
+//     message = 'what an usual age!'
+// }
+
+//Ex on non-traditional use of "?"
+// let company = prompt('Which company created JavaScript?', '');
+
+// (company == 'Netscape') ?
+//    alert('Right!') : alert('Wrong.');
+
+// Ex on how to rewrite if statement to ternary
+// if (a + b < 4){
+//    result = 'below';
+//     }    
+// else {
+//     result = 'over';
+//     }
+// alert (result)
+
+//Ex on AND operator
+// let hour = 12
+// let minutes = 30
+// if(hour == 12 && minutes == 30)
+// alert(hour + ":" +  minutes)
+
+//Ex on OR operator
+// alert( alert(1) || 2 || alert(3) );
+
+// let userName = prompt('who are you');
+// if (userName == 'Admin'){
+//     alert('success');
+// let pass = prompt('password', '');
+// if (pass == 'Themaster'){
+//     alert('welcome to our world!');
+// }else if (pass == "" || pass == null){
+//     alert ('canceled')
+// }else alert  ('wrong'); 
+// }else if (userName =="" || userName == null){
+//     alert ('canceled');
+// }else {
+//     alert('i dont know you');
+// }
+
+//Ex on While loop
+// let j = 0;
+// while (j < 5){
+// alert (j);
+// j++
+// }
+// for (let i =0; i < 20; i += 5){
+//     alert(i)
+// }
+
+// let a = 2 + 2;
+
+// switch (a) {
+//   case 3:
+//     alert( 'Too small' );
+//     break;
+//   case 4:
+//     alert( 'Exactly!' );
+//     break;
+//   case 5:
+//     alert( 'Too large' );
+//     break;
+//   default:
+//     alert( "I don't know such values" );
+// }
+
+// function pow(x, n) {
+//     let result = x;
+  
+//     for (let i = 1; i < n; i++) {
+//       result *= x;
+//     }
+  
+//     return result;
+//   }
+  
+//   let x = prompt("x?", '');
+//   let n = prompt("n?", '');
+  
+//   if (n < 1) {
+//     alert(`Power ${n} is not supported,
+//       use an integer greater than 0`);
+//   } else {
+//     alert( pow(x, n) );
+//   }
+// function BigUser() {
+
+//   this.name = "John";
+
+//   return { name: "Nnayelugo" };
+
+// alert( new BigUser().name );
+// }
+// function Calculator() {
+
+//   this.read = function() {
+//     this.a = +prompt('a?', 0);
+//     this.b = +prompt('b?', 0);
+//   };
+
+//   this.sum = function() {
+//     return this.a + this.b;
+//   };
+
+//   this.mul = function() {
+//     return this.a * this.b;
+//   };
+// }
+
+// let calculator = new Calculator();
+// calculator.read();
+
+// alert( "Sum=" + calculator.sum() );
+// alert( "Mul=" + calculator.mul() );
+ let num = 1.23456;
+
+ alert( Math.floor(num * 100) / 100 );
+ alert(num.toFixed(3))
